89a3d219b271016dd1a761fefff7872f02816dd6
sarusarang <sarangsaru445@gmail.com>
2024-08-27 16:28:13 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build' // Change the output directory to 'build'
-  }
+  
 })
